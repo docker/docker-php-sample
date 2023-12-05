b6290d511443206b0e33da4400b83580ec6ebd93
Craig Osterhout <103533812+craig-osterhout@users.noreply.github.com>
2023-12-05 12:29:28 -0800

    Update database.php for mariadb

--- src/database.php
+++ src/database.php
@@ -12,14 +12,14 @@ $password_file_path = getenv('PASSWORD_FILE_PATH');
 $db_pass = trim(file_get_contents($password_file_path));
 
 // Create a new PDO instance
-$db_handle = new PDO("pgsql:host=$db_host;dbname=$db_name", $db_user, $db_pass);
+$db_handle = new PDO("mysql:host=$db_host;dbname=$db_name", $db_user, $db_pass);
 
 // Create the "messages" table if it doesn't exist
 $db_handle->exec("
-  CREATE TABLE IF NOT EXISTS messages (
-      id SERIAL PRIMARY KEY,
-      message VARCHAR(255) NOT NULL
-  )
+ CREATE TABLE IF NOT EXISTS messages (
+     id INT AUTO_INCREMENT PRIMARY KEY,
+     message VARCHAR(255) NOT NULL
+ )
 ");
 
 // Create message
@@ -27,11 +27,11 @@ $stmt = $db_handle->query("SELECT COUNT(*) as count FROM messages");
 $row = $stmt->fetch(PDO::FETCH_ASSOC);
 $count = $row['count'];
 $db_handle->exec("
-  INSERT INTO messages (message)
-  SELECT CONCAT('message-', $count)
-  WHERE NOT EXISTS (
-    SELECT 1 FROM messages WHERE message = CONCAT('message-', $count)
-  )
+ INSERT INTO messages (message)
+ SELECT CONCAT('message-', '$count')
+ WHERE NOT EXISTS (
+  SELECT 1 FROM messages WHERE message = CONCAT('message-', '$count')
+ )
 ");
 
 // Retrieve all records from the "messages" table
@@ -39,7 +39,7 @@ $stmt = $db_handle->query("SELECT * FROM messages");
 
 // Print all records
 while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
-  echo $row['id'] . " " . $row['message'] . "<br>";
+ echo $row['id'] . " " . $row['message'] . "<br>";
 }
 
 // Close the database connection
